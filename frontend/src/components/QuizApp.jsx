53ab7bc190e4ddd4eb01400f6c9fe1e0befdced5
RajArya3347Y <raajaaryandewangan@gmail.com>
2025-03-04 20:41:33 +0530

    Changed frontend/styling

--- frontend/src/components/QuizApp.jsx
+++ frontend/src/components/QuizApp.jsx
@@ -20,11 +20,11 @@ function QuizApp(){
     }
 
     return(
-        <div className="bg-white h-[60%] w-[55%] rounded-xl p-3" id="quiz-container">
+        <div className="bg-white h-[60%] w-[55%] rounded-xl p-3 " id="quiz-container">
             <h1 className="text-[#0a2b5f] lg:text-3xl md:text-2xl sm:text-1xl font-bold lg:py-4 md:py-3 sm:py-2">To-Do App✏️</h1>
             <TaskField onTaskAdd={(val) => onTaskAdd(val)}/>
             <hr className="border-gray-400 m-4 border-[1px]" />
-            <ul className="py-3 px-11" id="taskList">
+            <ul className="py-3 px-11 max-h-[55%] overflow-auto"  id="taskList">
                 {tasks.map((task,index) => (
                     <Task 
                         taskName={task} 
